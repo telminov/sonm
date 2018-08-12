3a7fc556c2017765220390ec220520322904830a
sergey@telminov.ru <sergey@telminov.ru>
2018-08-12 21:12:09 +0300

    deal wrapper

--- sonm/deal.py
+++ sonm/deal.py
@@ -4,6 +4,32 @@ from . import CliMixin
 
 class Deal(CliMixin):
 
-    def list(self):
+    def list(self) -> dict:
         command_args = ['deal', 'list', '--out', 'json']
+        return self._call_command(command_args)
+
+    def open(self, ask_id: int, bid_id: int) -> dict:
+        command_args = ['deal', 'open', str(ask_id), str(bid_id), '--out', 'json']
+        return self._call_command(command_args)
+
+    def status(self, deal_id: int) -> dict:
+        command_args = ['deal', 'status', str(deal_id), '--out', 'json']
+        return self._call_command(command_args)
+
+    def close(self, deal_id: int) -> dict:
+        command_args = ['deal', 'close', str(deal_id), '--out', 'json']
+        return self._call_command(command_args)
+
+    def quick_buy(self, ask_id: int, duration: str = None) -> dict:
+        """
+        :param ask_id:
+        :param duration: 1h23m13s
+        """
+        command_args = ['deal', 'quick-buy', str(ask_id)]
+
+        if duration is not None:
+            command_args.append(str(duration))
+
+        command_args.extend(['--out', 'json'])
+
         return self._call_command(command_args)
